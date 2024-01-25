ed32d9c521c635579c3d94aeeb67c5c460b28664
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-26 01:18:11 +0800

    Define function for comparison of first and last number

--- check_if_the_first_and_last_number_of_a_list-is_the_same.py
+++ check_if_the_first_and_last_number_of_a_list-is_the_same.py
@@ -3,4 +3,14 @@
 # If numbers are different then return False.
 
 # Compare if the first and last number in the list is the same
+
+def first_and_last_comparison(numberList):
+    print("Given List: ", numberList)
+    
+    first_number = numberList[0]
+    last_number = numberList[-1]
+
+    same = first_number == last_number
+    return same
+
 # Print based on the given
