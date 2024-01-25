11cdec68fcba6fac6699c1ec756d226365bb2593
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-26 01:22:04 +0800

    Print results and given data

--- check_if_the_first_and_last_number_of_a_list-is_the_same.py
+++ check_if_the_first_and_last_number_of_a_list-is_the_same.py
@@ -5,7 +5,7 @@
 # Compare if the first and last number in the list is the same
 
 def first_and_last_comparison(numberList):
-    print("Given List: ", numberList)
+    print("\nGiven List: ", numberList)
     
     first_number = numberList[0]
     last_number = numberList[-1]
@@ -14,3 +14,9 @@ def first_and_last_comparison(numberList):
     return same
 
 # Print based on the given
+
+numbers_x = [10, 20, 30, 40, 10]
+print("Result is", first_and_last_comparison(numbers_x))
+
+numbers_y = [75, 65, 35, 75, 30]
+print("Result is", first_and_last_comparison(numbers_y))
