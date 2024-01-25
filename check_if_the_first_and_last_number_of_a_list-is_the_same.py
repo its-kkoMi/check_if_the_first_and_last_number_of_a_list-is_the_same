d4173fad083be68fa41222a257d3e2b1e1f9c8e8
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-26 01:15:09 +0800

    Program Instructions and Pseudocode

---
+++ check_if_the_first_and_last_number_of_a_list-is_the_same.py
@@ -0,0 +1,6 @@
+# Assignment 5 - Exercise 5
+# Write a function to return True if the first and last number of a given list is same. 
+# If numbers are different then return False.
+
+# Compare if the first and last number in the list is the same
+# Print based on the given
